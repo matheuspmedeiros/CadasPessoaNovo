22d4358ebaca457c10196a09fccc95cf46fd67bc
Matheus Paulino Medeiros <mpmedeiros@gmail.com>
2017-09-19 22:18:07 -0300

    19/09

---
+++ src/model/Pessoa.java
@@ -0,0 +1,125 @@
+package model;
+
+public class Pessoa {
+	String nome, endereco, bairro, cep, cidade, estado, telefone, celular, rg, cpf;
+	char sexo;
+	
+	//nome
+	public String getNome(){
+		return nome;
+	}
+	
+	
+	public void setNome (String nome){
+		this.nome = nome;
+	}
+	
+	//endereço
+	public String getEndereco(){
+		return endereco;
+	}
+	
+	
+	public void setEndereco (String endereco){
+		this.endereco = endereco;
+	}
+	
+	//bairro
+	
+	public String getBairro(){
+		return bairro;
+	}
+	
+	
+	public void setBairro (String bairro){
+		this.bairro = bairro;
+	}
+	
+	//cep
+	
+	public String getCep(){
+		return cep;
+	}
+	
+	
+	public void setCep (String cep){
+		this.cep = cep;
+	}
+	
+	//cidade
+	
+	public String getCidade(){
+		return cidade;
+	}
+	
+	
+	public void setCidade (String cidade){
+		this.cidade = cidade;
+	}
+	
+	//estado
+	
+	public String getEstado(){
+		return estado;
+	}
+	
+	
+	public void setEstado (String estado){
+		this.estado = estado;
+	}
+	
+	//telefone 
+	
+	public String getTelefone(){
+		return telefone;
+	}
+	
+	
+	public void setTelefone (String telefone){
+		this.telefone = telefone;
+	}
+	
+	//celular
+
+	public String getCelular(){
+		return celular;
+	}
+	
+	
+	public void setCelular (String celular){
+		this.celular = celular;
+	}
+	
+	//rg
+	
+	public String getRg(){
+		return rg;
+	}
+	
+	
+	public void setRg (String rg){
+		this.rg = rg;
+	}
+	
+	//cpf
+	
+	public String getCpf(){
+		return cpf;
+	}
+	
+	
+	public void setCpf (String cpf){
+		this.cpf = cpf;
+	}
+	
+	//sexo
+	
+	public char getSexo(){
+		return sexo;
+	}
+	
+	
+	public void setSexo (char sexo){
+		this.sexo = sexo;
+	}
+}
---
+++ src/view/Pessoa.java
@@ -0,0 +1,256 @@
+package view;
+		import java.awt.Container;
+		import java.awt.event.ActionEvent;
+		import java.awt.event.ActionListener;
+
+import javax.swing.JLabel;
+		import javax.swing.JRadioButton;
+		import javax.swing.ButtonGroup;
+		import javax.swing.JFrame;
+		import javax.swing.JTextField;
+		import javax.swing.JButton;
+		import javax.swing.JComboBox;
+		import javax.swing.JFormattedTextField;
+		import javax.swing.text.MaskFormatter;
+
+import view.Pessoa;
+		
+public class Pessoa extends JFrame {
+	
+	//caixa de texto
+	JTextField txtNome = new JTextField("");
+	JTextField txtEnde = new JTextField();
+	JTextField txtBairro = new JTextField();
+	JTextField txtCidade = new JTextField();
+	JTextField txtEstado = new JTextField();
+	
+	//label
+	JLabel lblInt = new JLabel ("Cadastro de Pessoa Fisica");
+	JLabel lblNome = new JLabel ("Nome :");
+	JLabel lblEnde = new JLabel ("Endereço :");
+	JLabel lblBairro = new JLabel ("Bairro :");
+	JLabel lblCep = new JLabel ("CEP :");
+	JLabel lblCidade = new JLabel ("Cidade :");
+	JLabel lblEstado = new JLabel ("Estado :");
+	JLabel lblTelefone = new JLabel ("Telefone :");
+	JLabel lblCelular = new JLabel ("Celular :");
+	JLabel lblRg = new JLabel ("RG :");
+	JLabel lblCpf = new JLabel ("CPF :");
+	
+	//botão
+	JButton btnSalvar = new JButton ("Salvar");
+	JButton btnImprimir = new JButton ("Imprimir");
+	
+	//sexo
+	JRadioButton rdnSexom = new JRadioButton ("Masculino") ;
+	JRadioButton rdnSexof = new JRadioButton ("Feminino");
+	ButtonGroup  grupo = new ButtonGroup ();
+	
+	//Estado
+	JComboBox cboEsta = new JComboBox();
+	
+	//mascara 
+	 
+	MaskFormatter formasTell = null;
+	JFormattedTextField txtTelefone = null;
+	
+	MaskFormatter formasCep = null;
+	JFormattedTextField txtCep = null;
+	
+	MaskFormatter formasCelular = null;
+	JFormattedTextField txtCelular = null;
+	
+	MaskFormatter formasRg = null;
+	JFormattedTextField txtRg = null;
+	
+	MaskFormatter formasCpf = null;
+	JFormattedTextField txtCpf = null;
+	
+	public Pessoa (){
+		
+		super("Cadatro Pessoa Fisica");
+		Container paine = this.getContentPane();
+		
+		//introdução
+			paine.add(lblInt);
+			lblInt.setBounds(120, 20, 200, 25);
+		
+		//Nome do cadastrado  
+			paine.add(lblNome);
+			lblNome.setBounds(20, 60, 65, 25);
+			
+			paine.add(txtNome);
+			txtNome.setBounds(100, 60, 200, 25);
+		
+		//Endereco do cadastrado
+			paine.add(lblEnde);
+			lblEnde.setBounds(20, 100, 65, 25);
+			
+			paine.add(txtEnde);
+			txtEnde.setBounds(100, 100, 200, 25);
+		
+		//Bairro do cadastrado 
+			paine.add(lblBairro);
+			lblBairro.setBounds(20, 140, 65, 25);
+			
+			paine.add(txtBairro);
+			txtBairro.setBounds(100, 140, 200, 25);
+		
+		//cep do cadastrado 
+			paine.add(lblCep);
+			lblCep.setBounds(20, 180, 65, 25);
+					
+				try {
+					formasCep = new MaskFormatter("#####-###");
+					txtCep = new JFormattedTextField(formasCep);
+				} catch (Exception ex){
+					ex.printStackTrace();
+				}
+			
+			paine.add(txtCep);
+			txtCep.setBounds(100, 180, 100, 25);
+				
+		//cidade do cadastrado 
+			paine.add(lblCidade);
+			lblCidade.setBounds(20, 220, 65, 25);
+			
+			paine.add(txtCidade);
+			txtCidade.setBounds(100, 220, 200, 25);
+		
+		//estado do cadastrado 
+			
+			paine.add(lblEstado);
+			lblEstado.setBounds(20, 260, 65, 25);
+			
+			paine.add(cboEsta);
+			cboEsta.setBounds(100, 260, 120, 25);
+			
+			//estados no cbo
+			
+				cboEsta.addItem("Acre");
+				cboEsta.addItem("Alagoas");
+				cboEsta.addItem("Amapa");
+				cboEsta.addItem("Amazonas");
+				cboEsta.addItem("Bahia");
+				cboEsta.addItem("Ceará");
+				cboEsta.addItem("Distrito Federal");
+				cboEsta.addItem("Espirito Santo");
+				cboEsta.addItem("Goiás");
+				cboEsta.addItem("Maranhão");
+				cboEsta.addItem("Mato Grosso");
+				cboEsta.addItem("Mato Grosso do Sul");
+				cboEsta.addItem("Minas Gerais");
+				cboEsta.addItem("Pará");
+				cboEsta.addItem("Paraíba");
+				cboEsta.addItem("Paraná");
+				cboEsta.addItem("Pernambuco");
+				cboEsta.addItem("Piauí");
+				cboEsta.addItem("Rio de Janeiro");
+				cboEsta.addItem("Rio Grande do Norte");
+				cboEsta.addItem("Rio Grande do Sul");
+				cboEsta.addItem("Rondônia");
+				cboEsta.addItem("Santa Catarina");
+				cboEsta.addItem("São Paulo");
+				cboEsta.addItem("Sergipe");
+				cboEsta.addItem("Tocantins");
+			
+		//Telefone do cadastrado 
+			paine.add(lblTelefone);
+			lblTelefone.setBounds(20, 300, 65, 25);
+			
+				try {
+					formasTell = new MaskFormatter("(##)####-####");
+					txtTelefone = new JFormattedTextField(formasTell);
+				} catch (Exception ex){
+					ex.printStackTrace();
+				}
+			
+			paine.add(txtTelefone);
+			txtTelefone.setBounds(100, 300, 100, 25);
+			
+		//Celuar do cadastrado 
+			paine.add(lblCelular);
+			lblCelular.setBounds(20, 340, 65, 25);
+			
+				try {
+					formasCelular = new MaskFormatter("(##)#####-####");
+					txtCelular = new JFormattedTextField(formasCelular);
+				} catch (Exception ex){
+					ex.printStackTrace();
+				}
+			
+			paine.add(txtCelular);
+			txtCelular.setBounds(100, 340, 100, 25);
+						
+		//Rg do cadastrado 
+			paine.add(lblRg);
+			lblRg.setBounds(20, 380, 65, 25);
+			
+				try {
+					formasRg = new MaskFormatter("##.###.###-A");
+					txtRg = new JFormattedTextField(formasRg);
+				} catch (Exception ex){
+					ex.printStackTrace();
+				}
+			
+			paine.add(txtRg);
+			txtRg.setBounds(100, 380, 100, 25);
+			
+		//Cpf do cadastrado 
+			paine.add(lblCpf);
+			lblCpf.setBounds(20, 420, 65, 25);
+			
+				try {
+					formasCpf = new MaskFormatter("###.###.###-##");
+					txtCpf = new JFormattedTextField(formasCpf);
+				} catch (Exception ex){
+					ex.printStackTrace();
+				}
+			
+			paine.add(txtCpf);
+			txtCpf.setBounds(100, 420, 100, 25);
+			
+		//rnd do sexo 
+			
+			grupo.add(rdnSexom);
+			grupo.add(rdnSexof);
+			
+			paine.add(rdnSexom);
+			rdnSexom.setBounds(20, 460, 100, 25);
+			
+			paine.add(rdnSexof);
+			rdnSexof.setBounds(120, 460, 80, 25);
+			
+		//btn limpar
+			
+			paine.add(btnImprimir);
+			btnImprimir.setBounds(180, 500, 80, 25);
+			/*btnLimpar.addActionListener(new ActionListener() {
+				@Override 
+					public void actionPerformed(ActionEvent e){
+						cboEsta.setSelectedItem("Null");
+						txtNome.setText("");
+						txtEnde.setText("");
+						txtBairro.setText("");
+						txtCep.setText("");
+						txtCidade.setText("");
+						txtTelefone.setText("");
+						txtCelular.setText("");
+						txtRg.setText("");
+						txtCpf.setText("");
+					}
+				});*/
+			//btn Enviar
+				paine.add(btnSalvar);
+				btnSalvar.setBounds(60, 500, 80, 25);
+			
+		paine.setLayout(null); // posicionamento dos objetos do layout
+		this.setSize(350,600); // tamanho da tela
+		this.setVisible(true);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+	
+	public static void main (String[] args){
+		Pessoa pes = new Pessoa();
+	}
+}
